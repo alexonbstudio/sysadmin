282d4ddf423c54aaa0b44512e65d055b98b2cb73
alexonbstudio <alexonbstudio@gmail.com>
2020-06-25 23:54:40 +0200

    Create automate-(sub)domain.sh

---
+++ host&name/automate-(sub)domain.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+##################################"
+##
+##		SCRIPTING BY alexonbstudio
+##################################"
+
+clear
+
+domain=$1
+ipv4local=$2
+ipv6local=$3
+quit=$4
+
+
+if [ ${whoami} != "root" || $USER = "root" ]; then
+	echo -e "YOUR 'RE NOT ROOT USER (sudo -i)"
+	sudo -i
+fi
+
+if [ -z $1 ]; then
+
+	hostnamectl set-hostname ${domain}
+
+	if [ -z $1 && -z $2 ]; then
+
+		echo "${ipv4local} ${domain}" >> /etc/hosts
+		
+	fi
+
+	if [ -z $1 && -z $3 ]; then
+
+		echo "${ipv4local} ${domain}" >> /etc/hosts
+		
+	fi	
+
+	if [ -z $4 ]; then
+
+		reboot now
+		
+	fi	
+fi
+
