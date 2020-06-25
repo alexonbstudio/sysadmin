4ae4b21f4a7f6dd6b77e369bb586eb88f1fce7b5
alexonbstudio <alexonbstudio@gmail.com>
2020-06-25 18:40:43 +0200

    Update apache.sh

--- web-server/apache.sh
+++ web-server/apache.sh
@@ -6,7 +6,7 @@ if [ ${whoami} != "root"  ]; then
 	apt-get install software-properties-common && add-apt-repository universe
 	echo -e "====== AUTO install NEED OPENSSL/APACHE/CERTBOT/MARIADB/ANTIVIRUS/FAIL2BAN/PHP for using CMS populare ======\n"
 	echo -e "\n\n====== CMS PHP Works WORDPRESS/JOOMLA/DRUPAL ======\n\n"
-	apt install curl zip unzip openssl apache2 certbot python3-certbot-apache mariadb-server php php-xml php-fpm php-cli php-curl php-mysql php-gd php-mbstring php-imagick php-intl php-xml php-zip php-cgi php-xmlrpc php-soap tidy php-tidy sqlite php-pear clamav clamav-daemon fail2ban -y
+	apt install curl zip unzip openssl apache2 certbot python3-certbot-apache mariadb-server php php-xml php-fpm php-cli php-curl php-mysql php-gd php-mbstring php-imagick php-intl php-xml php-zip php-cgi php-xmlrpc php-soap tidy php-tidy sqlite php-pear clamav clamav-daemon fail2ban net-tools -y
 	echo -e "\n\n====== The installation is done ======\n\n"
 	#apt purge && apt clean && apt autoremove
 	#clear
@@ -17,11 +17,11 @@ else
 	echo -e "\n====== CMS PHP Works WORDPRESS/JOOMLA/DRUPAL ======\n\n"
 	apt install curl zip unzip openssl apache2 certbot python3-certbot-apache mariadb-server php php-xml php-fpm php-cli php-curl php-mysql php-gd php-mbstring php-imagick php-intl php-xml php-zip php-cgi php-xmlrpc php-soap tidy php-tidy sqlite php-pear clamav clamav-daemon fail2ban -y
 	echo -e "\n\n====== The installation is done ======\n\n"
-	#apt purge && apt clean && apt autoremove
-<<<<<<< HEAD
-#	clear
+
+
 	
 fi
+	mysql_secure_installation
 
 	echo -e "\n\nSystemctl using easier recommandation\n\n"
 	systemctl enable apache2 && /lib/systemd/systemd-sysv-install enable apache2
@@ -31,10 +31,7 @@ fi
 #	if [ -z php7.4 ]; then
 #		
 #	fi
-#	if [ -z ifconfig ]; then
-#		apt install net-tools -y
 
-#	fi
 	
 	
 	echo -e "\n\nThe notice recommand\n\n"
@@ -43,8 +40,4 @@ fi
 	# Automate do the notice have
 	a2enmod proxy_fcgi setenvif ; a2enconf php7.4-fpm # ubuntu system version default
 	systemctl reload apache2
-=======
-	#clear
-	
-fi
->>>>>>> b63e9ec3a81c04cad432a88568f2ea01ee6eac78
+
