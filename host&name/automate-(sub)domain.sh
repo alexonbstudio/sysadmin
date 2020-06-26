d00f72709acbe7d8759f9bae6785fd1f4d83f048
alexonbstudio <alexonbstudio@gmail.com>
2020-06-26 21:07:56 +0200

    Update automate-(sub)domain.sh

--- host&name/automate-(sub)domain.sh
+++ host&name/automate-(sub)domain.sh
@@ -11,32 +11,41 @@ ipv4local=$2
 ipv6local=$3
 quit=$4
 
+	echo -e "##########			HELP STARTER			##########\n\n"
+	echo -e "\n\nStep 1) bash automate-(sub)domain.sh domain=alexonbstudio.tld #default domain\n\n"
+	echo -e "\n\nStep 2) bash automate-(sub)domain.sh ipv4local=x.x.x.x  domain=alexonbstudio.tld #ipv4local domain hosts\n\n"
+	echo -e "\n\nStep 3) bash automate-(sub)domain.sh ipv6local=x:x:::x:x  domain=alexonbstudio.tld #ipv6local domain hosts\n\n"
+	echo -e "\n\nStep 4) bash automate-(sub)domain.sh ipv6local=x:x:::x:x  quit # now reboot\n\n"
+	echo -e "\n\n##########			HELP END			##########"
 
-if [ ${whoami} != "root" || $USER = "root" ]; then
+if [ ${whoami} != "root" || $USER != "root" ]; then
+
 	echo -e "YOUR 'RE NOT ROOT USER (sudo -i)"
 	sudo -i
-fi
+	
+	if [ -z $1 ]; then
 
-if [ -z $1 ]; then
+		hostnamectl set-hostname ${domain}
 
-	hostnamectl set-hostname ${domain}
+		if [ -z $1 && -z $2 ]; then
 
-	if [ -z $1 && -z $2 ]; then
+			echo "${ipv4local} ${domain}" >> /etc/hosts
+			
+		fi
 
-		echo "${ipv4local} ${domain}" >> /etc/hosts
-		
+		if [ -z $1 && -z $3 ]; then
+
+			echo "${ipv6local} ${domain}" >> /etc/hosts
+			
+		fi	
+
+		if [ -z $4 ]; then
+
+			reboot now
+			
+		fi	
 	fi
-
-	if [ -z $1 && -z $3 ]; then
-
-		echo "${ipv4local} ${domain}" >> /etc/hosts
-		
-	fi	
-
-	if [ -z $4 ]; then
-
-		reboot now
-		
-	fi	
 fi
+		
+
 
