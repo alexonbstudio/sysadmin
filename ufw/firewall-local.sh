6dd888c3fae468b3adbe0ace3a23af3bbfbab04b
alexonbstudio <alexonbstudio@gmail.com>
2020-07-13 18:39:47 +0200

    Update firewall-local.sh

--- ufw/firewall-local.sh
+++ ufw/firewall-local.sh
@@ -5,20 +5,14 @@
 ufw allow from 123.123.123.123 to any port 22 proto tcp
 
 # loopback
-ufw allow in lo
-ufw allow out lo
+ufw allow lo
 
 # NAT
-ufw allow in nat
-ufw allow out nat
+ufw allow nat
 
 # DNS
-ufw allow in 53/tcp
-ufw allow out 53/tcp
-ufw allow in 53/udp
-ufw allow out 53/udp
+ufw allow 53
 
 
 # ICMP (Ping)
-ufw allow in icmp
-ufw allow out icmp
+ufw allow icmp
