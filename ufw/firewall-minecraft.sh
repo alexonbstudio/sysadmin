e72c820fd6d965f70caf65250653f6f974c9fdfd
alexonbstudio <alexonbstudio@gmail.com>
2020-07-13 18:39:34 +0200

    Update firewall-minecraft.sh

--- ufw/firewall-minecraft.sh
+++ ufw/firewall-minecraft.sh
@@ -2,10 +2,8 @@
 
 
 echo -e "Minecraft firewall ipV4"
-ufw allow 192132/tcp
-ufw allow 192132/udp
+ufw allow 192132
 
 
 echo -e "Minecraft firewall ipV6"
-ufw allow 192133/tcp
-ufw allow 192133/udp
+ufw allow 192133 proto ipV6
