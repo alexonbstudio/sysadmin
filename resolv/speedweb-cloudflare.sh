1b339532fa0bf013db62686dc79075eeec0b94b7
alexonbstudio <alexonbstudio@gmail.com>
2020-07-13 18:40:00 +0200

    Update speedweb-cloudflare.sh

--- resolv/speedweb-cloudflare.sh
+++ resolv/speedweb-cloudflare.sh
@@ -7,10 +7,13 @@
 
 
 ##### SPEED INTERNET WITH CLOUDFLARE BOTH IPv4 & IPv6
-if [ ${whoami} != "root" || $USER = "root"  ]; then
+if [ $whoami != "root" ]; then
 	sudo -i # not surly
-	echo "nameserver 1.1.1.1" >> /etc/resolv.conf && echo "nameserver 1.0.0.1" >> /etc/resolv.conf && echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf && echo "nameserver 2606:4700:4700::1001" >> /etc/resolv.conf  
+	echo "nameserver 1.1.1.1 \n
+	nameserver 1.0.0.1 \n
+	nameserver 2606:4700:4700::1111 \n
+	nameserver 2606:4700:4700::1001" >> /etc/resolv.conf  
 
 fi
-	echo "nameserver 1.1.1.1" >> /etc/resolv.conf && echo "nameserver 1.0.0.1" >> /etc/resolv.conf && echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.conf && echo "nameserver 2606:4700:4700::1001" >> /etc/resolv.conf
 
+echo "Require Root User do: sudo -i and run again sh ./speedweb-cloudflare.sh"
